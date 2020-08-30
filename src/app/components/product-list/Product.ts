27f0ab710df5c1c280c60a4a11bdebbf33fb1f4f
Brian <br.skrzynka@gmail.com>
2020-08-30 16:27:41 +0200

    add route to search by cathegory

---
+++ src/app/components/product-list/Product.ts
@@ -0,0 +1,26 @@
+export class Product {
+  public  id: number;
+  public sku: string;
+  public name: string;
+  public description: string;
+  public  unitPrice: number;
+  public imageUrl: string;
+  public active: boolean;
+  public unitsInStock: number;
+  public  dateCreated: Date;
+  public  dateUpdated: Date;
+
+  constructor(id: number, sku: string, name: string, description: string, unitPrice: number, imageUrl: string,
+              active: boolean, unitsInStock: number, dateCreated: Date, dateUpdated: Date) {
+    this.id = id;
+    this.sku = sku;
+    this.name = name;
+    this.description = description;
+    this.unitPrice = unitPrice;
+    this.imageUrl = imageUrl;
+    this.active = active;
+    this.unitsInStock = unitsInStock;
+    this.dateCreated = dateCreated;
+    this.dateUpdated = dateUpdated;
+  }
+}
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {Product} from './Product';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[];
+  currentCategoryId: number;
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
+    this.listProducts();
+  }
+  // tslint:disable-next-line:typedef
+  listProducts(){
+    const  hascathegoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if (hascathegoryId){
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      this.currentCategoryId = 1;
+    }
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+
+      }
+    );
+  }
+
+}
