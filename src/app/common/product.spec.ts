62ece56ba859f79d903b7f23b81d27e496cadb77
Brian <br.skrzynka@gmail.com>
2020-08-31 22:24:46 +0200

    add untracked files

---
+++ src/app/common/product.spec.ts
@@ -0,0 +1,7 @@
+// import { Product } from './product';
+//
+// describe('Product', () => {
+//   it('should create an instance', () => {
+//     expect(new Product("32","#2","32","323",22,"123321",true,2,)).toBeTruthy();
+//   });
+// });
